5565ea54f38e29676970d7349ced8f3e23a0ea73
harshita170802 <88373178+harshita170802@users.noreply.github.com>
2021-09-13 23:16:05 +0530

    Create Program3.cpp

---
+++ Week4/Program3.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+ 
+int partition(int arr[], int l, int r);
+int kthSmallest(int arr[], int l, int r, int k)
+{
+    if (k > 0 && k <= r - l + 1) {
+        int pos = partition(arr, l, r);
+        if (pos - l == k - 1)
+            return arr[pos];
+        if (pos - l > k - 1) 
+            return kthSmallest(arr, l, pos - 1, k);
+        return kthSmallest(arr, pos + 1, r, k - pos + l - 1);
+    }
+    return INT_MAX;
+}
+ 
+void swap(int* a, int* b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+int partition(int arr[], int l, int r)
+{
+    int x = arr[r], i = l;
+    for (int j = l; j <= r - 1; j++) {
+        if (arr[j] <= x) {
+            swap(&arr[i], &arr[j]);
+            i++;
+        }
+    }
+    swap(&arr[i], &arr[r]);
+    return i;
+}
+int main()
+{
+ int i,n,k,arr[10];
+     cin>>n;
+    for(i=0;i<n;i++){
+    	cin>>arr[i];
+	}; 
+	cin>>k;
+    cout << kthSmallest(arr, 0, n - 1, k);
+    return 0;
+}
